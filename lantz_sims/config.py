d536bb037d24423937c56f7dfe674c67a39abc23
Hernan Grecco <hernan.grecco@gmail.com>
2018-04-08 21:37:22 -0300

    Documented config.py

--- lantz_sims/config.py
+++ lantz_sims/config.py
@@ -15,5 +15,9 @@ from lantz_core.config import register_and_get
 # Configuration Values for lantz_sims
 # ====================================
 
+# Host of a TCP simulated instrument
 TCP_HOST = register_and_get('sims.tcp_host', 'localhost')
+
+# Port of a TCP simulated instrument
+# valid values: any value convertible to int
 TCP_PORT = int(register_and_get('sims.tcp_port', '5678'))

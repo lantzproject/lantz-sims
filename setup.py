fa1e820a8dfce8dc900dd557d3cf1e424e971df2
Hernan <hernan.grecco@gmail.com>
2019-01-21 16:21:30 -0300

    Back to development: 0.5.3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ __doc__ = long_description
 root_folder = os.path.dirname(os.path.abspath(__file__))
 
 setup(name='lantz-sims',
-      version='0.5.2',
+      version='0.5.3.dev0',
       license='BSD',
       description='Instrumentation framework',
       long_description=long_description,
